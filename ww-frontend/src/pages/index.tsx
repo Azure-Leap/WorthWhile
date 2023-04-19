9a57249db14bfc1bc23d376196bd70319b2389b6
AzureDuulga <azure.duulga.enkhtur@gmail.com>
2023-04-19 13:27:58 +0800

    homepage update

--- ww-frontend/src/pages/index.tsx
+++ ww-frontend/src/pages/index.tsx
@@ -4,6 +4,34 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export interface IItem {
+  title: string;
+  BgURL: string;
+}
+
+const categories: IItem[] = [
+  {
+    title: "Hair",
+    BgURL:
+      "https://images.unsplash.com/photo-1614838000027-76439aa4aedc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGhhaXIlMjBjdXR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    title: "Nail",
+    BgURL:
+      "https://images.unsplash.com/photo-1632345031435-8727f6897d53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmFpbCUyMHNhbG9ufGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    title: "Skin",
+    BgURL:
+      "https://images.unsplash.com/photo-1512290923902-8a9f81dc236c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZmFjaWFsfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    title: "Makeup",
+    BgURL:
+      "https://images.unsplash.com/photo-1613966802194-d46a163af70d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFrZXVwJTIwYXJ0aXN0fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
+
 export default function Index() {
   return (
     <>
@@ -16,7 +44,11 @@ export default function Index() {
         <b>Discover yourself</b>
         <SearchComponent />
       </div>
-      <HomeCard />
+      <div className="flex gap-4 justify-center h-full w-4/5 flex-wrap">
+        {categories?.map((el, idx) => {
+          return <HomeCard key={idx} item={el} />;
+        })}
+      </div>
     </>
   );
 }
