e8f6a27e27e1ff504b270ae7bca107a911d15e74
Evening <evening@mstars-lab3-05s-iMac.local>
2023-04-24 19:56:26 +0800

    font

--- ww-frontend/src/pages/_app.tsx
+++ ww-frontend/src/pages/_app.tsx
@@ -3,15 +3,20 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import AuthProvider from "@/context/authContext";
 import AlertProvider from "@/context/alertContext";
+import { Lato } from "@next/font/google";
+
+const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AlertProvider>
-      <AuthProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </AuthProvider>
-    </AlertProvider>
+    <main className={lato.className}>
+      <AlertProvider>
+        <AuthProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </AuthProvider>
+      </AlertProvider>
+    </main>
   );
 }
