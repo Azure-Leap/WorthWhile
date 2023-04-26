e65770c2f70b29f12394474a7e3a8b20d3e5f944
AzureDuulga <azure.duulga.enkhtur@gmail.com>
2023-04-26 19:49:45 +0800

    bsuiness auth

--- ww-frontend/src/components/BusinessAuth/SignUp/businessHours.tsx
+++ ww-frontend/src/components/BusinessAuth/SignUp/businessHours.tsx
@@ -1,3 +1,4 @@
+import { border } from "@mui/system";
 import { useState } from "react";
 import { FiToggleLeft, FiToggleRight } from "react-icons/fi";
 
@@ -9,29 +10,23 @@ type Day = {
 };
 
 const days: Day[] = [
-  { name: "Monday", isOpen: false, startTime: "09:00", endTime: "17:00" },
-  { name: "Tuesday", isOpen: false, startTime: "09:00", endTime: "17:00" },
-  { name: "Wednesday", isOpen: false, startTime: "09:00", endTime: "17:00" },
-  { name: "Thursday", isOpen: false, startTime: "09:00", endTime: "17:00" },
-  { name: "Friday", isOpen: false, startTime: "09:00", endTime: "17:00" },
-  { name: "Saturday", isOpen: false, startTime: "09:00", endTime: "17:00" },
-  { name: "Sunday", isOpen: false, startTime: "09:00", endTime: "17:00" },
+  { name: "Monday", isOpen: true, startTime: "09:00", endTime: "17:00" },
+  { name: "Tuesday", isOpen: true, startTime: "09:00", endTime: "17:00" },
+  { name: "Wednesday", isOpen: true, startTime: "09:00", endTime: "17:00" },
+  { name: "Thursday", isOpen: true, startTime: "09:00", endTime: "17:00" },
+  { name: "Friday", isOpen: true, startTime: "09:00", endTime: "17:00" },
+  { name: "Saturday", isOpen: true, startTime: "09:00", endTime: "17:00" },
+  { name: "Sunday", isOpen: true, startTime: "09:00", endTime: "17:00" },
 ];
 
 const BusinessHourInput = () => {
-  const [checked, setChecked] = useState(true);
-
-  const toggle = () => {
-    const newChecked = !checked;
-    setChecked(newChecked);
-  };
-
   const [weekDays, setWeekDays] = useState(days);
-
-  const toggleDay = (index: number) => {
-    const newDays = [...weekDays];
-    newDays[index].isOpen = !newDays[index].isOpen;
-    setWeekDays(newDays);
+  const toggleDay = (index: any) => {
+    let selectedDay = weekDays[index];
+    selectedDay.isOpen = selectedDay.isOpen === false ? true : false;
+    let temp = [...weekDays];
+    temp[index] = selectedDay;
+    setWeekDays(temp);
   };
 
   const setTime = (
@@ -39,58 +34,72 @@ const BusinessHourInput = () => {
     key: "startTime" | "endTime",
     value: string
   ) => {
-    const newDays = [...weekDays];
+    let newDays = [...weekDays];
     newDays[index][key] = value;
     setWeekDays(newDays);
   };
 
   return (
-    <div className="flex flex-col space-y-4">
-      <div
-        className={`flex items-center w-14 h-8 rounded-full px-1 ${
-          checked ? "bg-green-500" : "bg-gray-300"
-        }`}
-        onClick={toggle}
-      >
-        <div
-          className={`bg-white w-6 h-6 rounded-full shadow-md transform transition-transform ${
-            checked ? "translate-x-6" : ""
-          }`}
-        />
-      </div>
-      {weekDays.map((day, index) => (
-        <div key={day.name} className="flex items-center justify-between">
-          <span className="font-medium">{day.name}</span>
-          <div className="flex items-center space-x-4">
-            <button
-              className={`px-2 py-1 rounded ${
-                day.isOpen
-                  ? "bg-green-500 text-white"
-                  : "bg-gray-200 text-gray-600"
-              }`}
-              onClick={() => toggleDay(index)}
-            >
-              {day.isOpen ? <FiToggleRight /> : <FiToggleLeft />}
-            </button>
-            {day.isOpen && (
-              <div className="flex space-x-2">
-                <input
-                  type="time"
-                  value={day.startTime}
-                  onChange={(e) => setTime(index, "startTime", e.target.value)}
-                />
-                <span>to</span>
-                <input
-                  type="time"
-                  value={day.endTime}
-                  onChange={(e) => setTime(index, "endTime", e.target.value)}
+    <>
+      <h2 className="text-3xl font-medium">Your Business Hours</h2>
+      <p className="text-xs text-gray-500">When can clients book with you?</p>
+      <div className="flex flex-col text-gray-900">
+        {weekDays.map((day, index) => (
+          <div
+            key={day.name}
+            className={`flex items-center justify-between py-4 gap-7 ${
+              day.name === "Sunday"
+                ? "border-y-2 border-gray-100"
+                : " border-t-2 border-gray-100"
+            }`}
+          >
+            <span className="flex items-center gap-3">
+              <div
+                className={`flex items-center w-14 h-8 rounded-full px-1 ${
+                  day.isOpen ? "bg-green-500" : "bg-gray-300"
+                }`}
+                onClick={(e) => toggleDay(index)}
+              >
+                <div
+                  className={`bg-white w-6 h-6 rounded-full shadow-md transform transition-transform ${
+                    day.isOpen ? "translate-x-6" : ""
+                  }`}
                 />
               </div>
-            )}
+              <p className="font-medium">{day.name}</p>
+            </span>
+            {/* Times starts from here */}
+            <div className="flex items-center space-x-4">
+              {day.isOpen ? (
+                <div className="flex space-x-2">
+                  <input
+                    type="time"
+                    value={day.startTime}
+                    onChange={(e) =>
+                      setTime(index, "startTime", e.target.value)
+                    }
+                  />
+                  <span>-</span>
+                  <input
+                    type="time"
+                    value={day.endTime}
+                    onChange={(e) => setTime(index, "endTime", e.target.value)}
+                  />
+                </div>
+              ) : (
+                <p>Closed</p>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-gray-800 text-white font-semibold py-2 px-4 rounded-lg shadow text-xs"
+      >
+        CONTINUE
+      </button>
+    </>
   );
 };
 
--- ww-frontend/src/components/BusinessAuth/SignUp/signup.tsx
+++ ww-frontend/src/components/BusinessAuth/SignUp/signup.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import AboutYouAuth from "./aboutYou";
 import AddressAuth from "./address";
+import AddServiceAuth from "./addService/addService";
+import AddStaffAuth from "./addStaff";
 import BusinessHourInput from "./businessHours";
 import EmailAuth from "./email";
 import PasswordAuth from "./password";
+import { FiArrowLeft } from "react-icons/fi";
 
 const BusinessSignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [progress, setProgress] = useState(90);
 
   const handleSignIn = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -15,16 +19,30 @@ const BusinessSignUp = () => {
   };
   return (
     <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center bg-gray-100 ">
-      <div className="flex w-1/3 justify-center items-center bg-white p-7 rounded-md shadow-sm">
-        <form
-          className="mt-8 space-y-6 w-full max-w-md"
-          onSubmit={handleSignIn}
-        >
-          {/* <EmailAuth /> */}
-          {/* <AboutYouAuth />  */}
-          {/* <PasswordAuth /> */}
-          {/* <AddressAuth /> */}
+      <div className="w-fit items-center bg-white p-7 rounded-md shadow-sm">
+        <div className="flex items-center border-b-2 pb-6">
+          <FiArrowLeft size={30} color="gray" />
+          <div className="w-1/3 bg-gray-300 rounded-full">
+            <div
+              className="h-3 rounded-full bg-green-400"
+              style={{ width: `${progress}%` }}
+            />
+          </div>
+        </div>
+        <form className="mt-6 space-y-6 max-w-md" onSubmit={handleSignIn}>
+          <EmailAuth />
+          <p>End Here</p>
+          {/* <AboutYouAuth />
+          <p>End Here</p>
+          <PasswordAuth />
+          <p>End Here</p>
+          <AddressAuth />
+          <p>End Here</p>
           <BusinessHourInput />
+          <p>End Here</p>
+          <AddServiceAuth />
+          <p>End Here</p>
+          <AddStaffAuth /> */}
         </form>
       </div>
     </main>
