e79b42f6faa9ec558c885bf41630defaaa4df84a
AzureDuulga <azure.duulga.enkhtur@gmail.com>
2023-04-27 17:02:37 +0800

    Business page updated

---
+++ ww-frontend/src/pages/business/SalonDetail/index.tsx
@@ -0,0 +1,8 @@
+import BusinessLayout from "@/components/BusinessLayout";
+import React from "react";
+
+const SalonDetail = () => {
+  return <BusinessLayout>SalonDetail</BusinessLayout>;
+};
+
+export default SalonDetail;
---
+++ ww-frontend/src/pages/business/Staff/index.tsx
@@ -0,0 +1,8 @@
+import BusinessLayout from "@/components/BusinessLayout";
+import React from "react";
+
+const Staff = () => {
+  return <BusinessLayout>Staff</BusinessLayout>;
+};
+
+export default Staff;
