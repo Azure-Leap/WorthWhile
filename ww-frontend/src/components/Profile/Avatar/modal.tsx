4da59f9d34dfe9cc0c3afb3a6624b93875f49a65
MisterAshid <tsekeashid@gmail.com>
2023-05-09 18:01:10 +0800

    img upload change(not done0, added default img BE

--- ww-frontend/src/components/Profile/Avatar/modal.tsx
+++ ww-frontend/src/components/Profile/Avatar/modal.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { AuthContext } from "@/context/authContext";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
@@ -7,6 +8,7 @@ import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import dynamic from "next/dynamic";
+import axios from "axios";
 
 const Avatar = dynamic(() => import("react-avatar-edit"), {
   ssr: false,
@@ -27,10 +29,12 @@ const style = {
 const ImportImage = ({ setAvatarUrl }: any) => {
   const [open, setOpen] = React.useState(false);
 
+  const { user, setUser } = useContext(AuthContext);
+  const [img, setImg] = useState(false);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const [src, setSrc] = useState(null);
   const [preview, setPreview] = useState("");
 
   const onClose = () => {
@@ -40,9 +44,14 @@ const ImportImage = ({ setAvatarUrl }: any) => {
     setPreview(view);
   };
 
-  // useEffect(() => {
-  //   console.log(preview);
-  // }, [preview]);
+  const add = async () => {
+    try {
+      const res = await axios.post(`http://localhost:8888/zuragUploadHiinee`);
+      setUser(res.data.user);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
 
   return (
     <div className="absolute left-0 top-6">
@@ -54,41 +63,106 @@ const ImportImage = ({ setAvatarUrl }: any) => {
       >
         <PhotoCamera sx={{ width: 20 }} />
       </IconButton>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Change Avatar
-          </Typography>
-          <Box sx={{ marginTop: 5, marginBottom: 5 }}>
-            <Avatar
-              width={330}
-              height={300}
-              onCrop={onCrop}
-              onClose={onClose}
-            />
-            {/* <img src={preview} alt="Alt" /> */}
-          </Box>
-          <Stack direction="row" spacing={2}>
-            <Button variant="outlined">Delete</Button>
-            <Button
-              sx={{ color: "white" }}
-              variant="contained"
-              onClick={() => {
-                setAvatarUrl(preview);
-                console.log(preview);
-                handleClose();
+      {img ? (
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
+              sx={{ borderBottom: 1, borderColor: "gray" }}
+            >
+              Change Avatar
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: 1,
+                marginBottom: 2,
               }}
             >
-              Save
-            </Button>
-          </Stack>
-        </Box>
-      </Modal>
+              <Box
+                sx={{
+                  width: 275,
+                  height: 300,
+                  marginTop: 1,
+                  marginBottom: 6,
+                }}
+              >
+                <img src={user?.profileImg} alt="userImg" />
+              </Box>
+            </Box>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ justifyContent: "center" }}
+            >
+              <Button
+                variant="outlined"
+                color="error"
+                onClick={() => {
+                  handleClose();
+                  setImg(false);
+                }}
+              >
+                Delete
+              </Button>
+              <Button
+                sx={{ color: "white" }}
+                variant="contained"
+                onClick={() => {
+                  handleClose();
+                }}
+              >
+                Save
+              </Button>
+            </Stack>
+          </Box>
+        </Modal>
+      ) : (
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Change Avatar
+            </Typography>
+            <Box sx={{ marginTop: 5, marginBottom: 5 }}>
+              <Avatar
+                width={330}
+                height={300}
+                onCrop={onCrop}
+                onClose={onClose}
+              />
+            </Box>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ justifyContent: "center" }}
+            >
+              <Button
+                sx={{ color: "white" }}
+                variant="contained"
+                onClick={() => {
+                  setAvatarUrl(preview);
+                  handleClose();
+                }}
+              >
+                Add
+              </Button>
+            </Stack>
+          </Box>
+        </Modal>
+      )}
     </div>
   );
 };
--- ww-frontend/src/pages/profile/giftcards/index.tsx
+++ ww-frontend/src/pages/profile/giftcards/index.tsx
@@ -1,28 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import SideLayout from "@/components/SideLayout";
+import EmptyGiftCard from "./empty";
+import GiftCard from "./giftCard";
 
-const Profile = () => {
+interface IGiftCard {
+  price: number;
+  amount: number;
+  cardNumber: number;
+}
+
+export default function App() {
+  const [apps, setApps] = useState<IGiftCard[]>([
+    {
+      price: 10,
+      amount: 11,
+      cardNumber: 11,
+    },
+    {
+      price: 10,
+      amount: 11,
+      cardNumber: 11,
+    },
+  ]);
+
   return (
     <SideLayout>
-      <div className="ml-5 mt-2">
-        <div>
-          <h1 className="text-2xl ">Gift Cards</h1>
-        </div>
-        <div className="my-20">
-          <img
-            src="/image/profile/giftCard.png"
-            alt="review"
-            className="w-1/6 h-1/6 mx-auto"
-          />
-          <h2 className="text-xl text-center my-5 ">Your Gift Cards</h2>
-          <p className="text-gray-500 text-center">
-            Find them here! You can get a gift card from a friend or buy one
-            yourself.
-          </p>
-        </div>
-      </div>
+      {apps.length ? <GiftCard apps={apps} /> : <EmptyGiftCard />}
     </SideLayout>
   );
-};
-
-export default Profile;
+}
