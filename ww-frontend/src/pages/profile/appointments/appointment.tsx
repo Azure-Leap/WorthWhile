22264d2c1890d7eb261c0b294fb31302743a4596
Monkhzul Bayarkhuu <monkhzulbayarkhuu@Monkhzuls-MacBook-Air.local>
2023-05-17 17:16:12 +0800

    Build aldaa zaslaa

--- ww-frontend/src/pages/profile/appointments/appointment.tsx
+++ ww-frontend/src/pages/profile/appointments/appointment.tsx
@@ -17,44 +17,45 @@ const Appointments = ({ apps }: { apps: IAppointments[] }) => {
         <h2 className="my-5">Finished Appointments</h2>
       </div>
       <div>
-        {apps.map((app: IAppointments, i: number) => (
-          <div
-            key={i}
-            className="flex my-10 border-gray-300 border-2 rounded-xl p-5 min-w-full shadow-xl "
-          >
-            <div className="border-r-2 w-96">
-              <h1 className="text-2xl font-bold">{app.services}</h1>
-              <div className="flex my-2">
-                <Avatar
-                  sx={{
-                    width: 25,
-                    height: 25,
-                  }}
-                  alt="Ashid"
-                  src=""
-                />
-                <h3 className="pl-2">{app.barber}</h3>
-              </div>
+        {apps &&
+          apps.map((app: IAppointments, i: number) => (
+            <div
+              key={i}
+              className="flex my-10 border-gray-300 border-2 rounded-xl p-5 min-w-full shadow-xl "
+            >
+              <div className="border-r-2 w-96">
+                <h1 className="text-2xl font-bold">{app.services}</h1>
+                <div className="flex my-2">
+                  <Avatar
+                    sx={{
+                      width: 25,
+                      height: 25,
+                    }}
+                    alt="Ashid"
+                    src=""
+                  />
+                  <h3 className="pl-2">{app.barber}</h3>
+                </div>
 
-              <Button
-                sx={{
-                  bgcolor: "lime",
-                  color: "white",
-                  paddingLeft: 3,
-                  paddingRight: 3,
-                  ":hover": {
-                    bgcolor: "green",
-                  },
-                }}
-              >
-                Book Again
-              </Button>
+                <Button
+                  sx={{
+                    bgcolor: "lime",
+                    color: "white",
+                    paddingLeft: 3,
+                    paddingRight: 3,
+                    ":hover": {
+                      bgcolor: "green",
+                    },
+                  }}
+                >
+                  Book Again
+                </Button>
+              </div>
+              <div className="pl-5 flex items-center">
+                {dayjs(app.startTime).format("YYYY-MM-DD HH:mm")}
+              </div>
             </div>
-            <div className="pl-5 flex items-center">
-              {dayjs(app.startTime).format("YYYY-MM-DD HH:mm")}
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
